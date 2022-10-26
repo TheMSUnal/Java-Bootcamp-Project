b201732cbdfc971c2911797026122f366a2477a0
TheMSUnal <muhsalihunal@gmail.com>
2022-10-26 15:26:32 +0300

    oopwithNlayeredApp

---
+++ threeweek/youtube33-45/oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -0,0 +1,13 @@
+package oopWithNLayeredApp.dataAccess;
+
+import oopWithNLayeredApp.entities.Product;
+
+public class HibernateProductDao  implements ProductDao {
+	 public void add(Product product) {
+    	 System.out.println("Hibarnate ile veritabanına eklendi");
+}
+
+	
+		
+	}
+
